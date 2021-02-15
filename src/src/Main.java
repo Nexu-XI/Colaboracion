57fb4a7fa3cea1ce9e2fa8664f562f27390fd74b
Nexu <alexandrunicolae.neacsu@campusfp.es>
2021-02-15 09:33:06 +0100

    Rama Principal

---
+++ src/src/Main.java
@@ -0,0 +1,18 @@
+package src;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        
+    }
+    
+    public static void pitosuma(){
+        
+    }
+    
+    public static void pitoresta(){
+        
+    }
+}
